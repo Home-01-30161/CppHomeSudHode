1754fcdbf343888a236a7365c61001ba5076d631
home-01-30161 <h63966239@gmail.com>
2026-07-18 21:31:16 +0700

    วาร์ปไปตอนเป็นนิสิตเลยได้ไหม T-T

---
+++ oven.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+	string s;
+	int x=0;
+	cin>>s;
+	for(int i=0; i<s.size(); i++){
+		cout<<s[i]<<" ";
+		int current_val=s[i]-'0';
+		if(i%4==0){
+			x+=current_val;
+		}
+		else if(i%4==1){
+			x-=current_val;
+		}
+		else if(i%4==2){
+			x*=current_val;
+		}
+		else {
+			x/=current_val;
+		}
+	}
+	cout<<x;		
+}
